6b3f9249608514da8ee50c67b77571d1f8b70ae9
Código Uranio <codigouranio@gmail.com>
2026-04-27 17:39:35 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_not_works() {
+        let result = add(2, 3);
+        assert_ne!(result, 1);
+    }
+}
